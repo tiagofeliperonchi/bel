1702f14ed8b527f0b5c6e5fee381d95a13fa3557
Tiago Felipe <tiago995@gmail.com>
2023-06-07 13:27:48 -0300

    Ajustes

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,30 +1,55 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-
-    z = document.querySelectorAll(['[include]']);
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute('include');
-        xhttp = new XMLHttpRequest();
+    const z = document.querySelectorAll(['[include]'])
+    for (let i = 0; i < z.length; i++) {
+        const elmnt = z[i]
+        const file = elmnt.getAttribute('include')
+        const xhttp = new XMLHttpRequest()
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    elmnt.innerHTML = this.responseText;
+                    elmnt.innerHTML = this.responseText
                 }
                 if (this.status == 404) {
-                    elmnt.innerHTML = 'Page not found.';
+                    elmnt.innerHTML = 'Page not found.'
                 }
 
-                elmnt.removeAttribute('include');
-                includeHTML();
+                elmnt.removeAttribute('include')
+                includeHTML()
             }
         }
-        xhttp.open('GET', file, true);
-        xhttp.send();
+        xhttp.open('GET', file, true)
+        xhttp.send()
 
-        return;
-
+        return
     }
 }
-
 includeHTML()
+
+$(function () {
+    $('body').on('show.bs.collapse', '#main-menu', function () {
+        $('body').addClass('menu-opening overlay')
+    }).on('shown.bs.collapse', function () {
+        $('body').addClass('menu-open').removeClass('menu-opening')
+    }).on('hide.bs.collapse', function () {
+        $('body').addClass('menu-closing').removeClass('menu-open')
+    }).on('hidden.bs.collapse', function () {
+        $('body').removeClass('menu-open overlay menu-closing')
+    })
+    const colorLegend = $('<div id="circle-legend" />')
+
+    $('body').append(colorLegend)
+
+    $('#circle .item').on('mousemove', function (event) {
+        colorLegend.text($(this).text())
+
+        const left = $(this).data('direct') == 'left' ? (event.pageX + 10) : event.pageX - colorLegend.innerWidth()
+        colorLegend.css({
+            'background': $(this).data('color'),
+            display: 'flex',
+            top: event.pageY,
+            left: left
+        })
+    }).on('mouseleave', function () {
+        colorLegend.hide()
+    })
+})
